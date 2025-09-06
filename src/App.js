dac675cdca0818f878f8a64a9f16dc2118524fa7
vivekprajapati500 <prajapativivek500@gmail.com>
2025-09-06 18:40:13 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is demo project for React CI/CD pipeline.
+          This is demo project for React CI/CD pipeline app.
         </p>
         <a
           className="App-link"
